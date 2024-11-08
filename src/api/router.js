007007c34a4bf9918d2d904b0abb3ea41f6f9bdf
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2024-11-08 12:36:13 -0300

    fix(api): remove the `/` route that tested a `hello world` message

--- src/api/router.js
+++ src/api/router.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const { sendMessage, sendHello } = require("./controllers/messages.controller");
-const {
-  validateMessageBody,
-  validateHello,
-} = require("./middlewares/messages.middlewares");
+const { sendMessage } = require("./controllers/messages.controller");
+const { validateMessageBody } = require("./middlewares/messages.middlewares");
 const { getGoldValue } = require("./controllers/gold.controller");
 
 const createRouter = (socket) => {
   const router = express.Router();
-
-  router.get("/", validateHello, sendHello);
 
   router.post("/send-message", validateMessageBody, (req, res) => {
     sendMessage(req, res, socket);
